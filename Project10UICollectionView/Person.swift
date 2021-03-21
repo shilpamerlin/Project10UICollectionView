de74ffbc7a01b003b16c25676bf175d1073e512f
Shilpa <shilpamjoy@gmail.com>
2021-03-21 14:20:56 -0400

    Done with UICollectionView and UIImage PickerView

---
+++ Project10UICollectionView/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10UICollectionView
+//
+//  Created by Shilpa Joy on 2021-03-21.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project10UICollectionView/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10UICollectionView
+//
+//  Created by Shilpa Joy on 2021-03-21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+
+    
+}
